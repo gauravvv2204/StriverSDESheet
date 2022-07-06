af4824464638c5a92baaeaa5d30c6ac359f290f1
Gaurav Pahwa <81811468+gauravvv2204@users.noreply.github.com>
2022-07-06 15:56:22 +0530

    Create MergeKSortedArrays.java

---
+++ Day 12/MergeKSortedArrays.java
@@ -0,0 +1,19 @@
+//https://www.codingninjas.com/codestudio/problems/merge-k-sorted-arrays_975379?leftPanelTab=1
+import java.util.*;
+public class Solution 
+{
+	public static ArrayList<Integer> mergeKSortedArrays(ArrayList<ArrayList<Integer>> kArrays, int k)
+	{
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<kArrays.size(); i++){
+            for(int j=0; j<kArrays.get(i).size(); j++){
+                pq.add(kArrays.get(i).get(j));
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(!pq.isEmpty()){
+            ans.add(pq.poll());
+        }
+        return  ans;
+	}
+}
